3cb16358dcc3755788cf2799036d9dcb4d56ba47
b-xb <7443583+b-xb@users.noreply.github.com>
2023-05-07 01:19:55 +0100

    Add route structure

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+const productsRouter = require('./products.js');
+apiRouter.use('/products', productsRouter);
+
+const ordersRouter = require('./orders.js');
+apiRouter.use('/orders', ordersRouter);
+
+const usersRouter = require('./users.js');
+apiRouter.use('/users', usersRouter);
+
+const authRouter = require('./auth.js');
+apiRouter.use('/auth', authRouter);
+
 apiRouter.get("/", (req, res) => {
   res.status(204).send();
 });
