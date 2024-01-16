2720dc26aba02160889ea7864b2812c52fa357cd
b-xb <7443583+b-xb@users.noreply.github.com>
2024-01-16 17:55:47 +0000

    Return empty product list if error retrieving products

--- view/src/api/products.js
+++ view/src/api/products.js
@@ -2,9 +2,13 @@ import { API_ENDPOINT } from ".";
 
 
 export const getProducts = async () => {
-  const response = await fetch(`${API_ENDPOINT}/products`);
-  const responseData = await response.json();
-  return responseData;
+  try {
+    const response = await fetch(`${API_ENDPOINT}/products`);
+    const responseData = await response.json();
+    return responseData;
+  } catch {
+    return [];
+  }
 };
 
 export const getProductById = async (productId) => {
